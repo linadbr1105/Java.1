63ab45f20ed48cd75401b084e5aba97fe49e1c6f
linadbr <linadbr1105@naver.com>
2021-09-28 04:27:58 +0900

    Start project

---
+++ Hello.java
@@ -0,0 +1,15 @@
+//작성자:J
+/*
+작성일: 2021.0.26.
+목  적: 첫 자바 프로그램 개발 방법을 공부
+*/
+
+//자바 소스파일, 주석
+public class Hello {
+  //프로그램 실행 진입점
+  public static void main(String[] args){
+    //콘솔에 출력하는 실행문
+    System.out.println("Hello, welcome to the java world!");
+  }//end of main
+}//end of class
+
---
+++ RunStatementExample.java
@@ -0,0 +1,9 @@
+//실행문
+public class RunStatementExample {
+  public static void main(String[] args){
+    int x = 1;
+    int y = 2;
+    int result = x+y;
+    System.out.println(x + "+" + y + "=" + result);
+  }
+}
